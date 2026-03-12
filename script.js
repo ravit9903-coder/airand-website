0e421f7dae6b4fd1865a6978353b6f93db5ee7e9
Ravi Teja <ravit9903@gmail.com>
2026-03-12 12:41:56 +0530

    AIRANDD AI website

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ behavior:"smooth"
 });
 
 }
+
+function sendMessage(){
+
+let input=document.getElementById("chatInput");
+let message=input.value;
+
+let chat=document.getElementById("chatBody");
+
+chat.innerHTML+= "<div>User: "+message+"</div>";
+
+let reply="AI Assistant: Thank you for contacting AIRANDD.";
+
+chat.innerHTML+= "<div>"+reply+"</div>";
+
+input.value="";
+
+}
